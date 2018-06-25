cf400891d0da87151097027554342bd3d176b3f8
FDws <885374150@qq.com>
2018-06-25 09:12:22 +0800

    init springboot project

---
+++ src/main/java/org/zc/homerent/HomerentApplication.java
@@ -0,0 +1,12 @@
+package org.zc.homerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomerentApplication.class, args);
+    }
+}
